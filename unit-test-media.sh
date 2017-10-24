d7f3e9a98c8582f030e73dd49baec9c3069f80e3
sitkevij <sitkevij@gmail.com>
2017-10-23 21:49:09 -0700

    adjust unit-test-mediag.sh pwd

--- unit-test-media.sh
+++ unit-test-media.sh
@@ -9,10 +9,10 @@ dir=/media
 echo "** get $file1"
 curl -LOs https://raw.githubusercontent.com/sitkevij/test-media/master/media/$file1 > $file1
 echo "** transcode $file1 to $size $file2"
-docker run --rm -v $(PWD):$dir sitkevij/ffmpeg -i $dir/$file1 -c:v libvpx-vp9 -b:v $size -c:a libvorbis $dir/$file2 -y
+docker run --rm -v $(pwd):$dir sitkevij/ffmpeg -i $dir/$file1 -c:v libvpx-vp9 -b:v $size -c:a libvorbis $dir/$file2 -y
 echo "** transcode $file1 to yuv"
-docker run --rm -v $(PWD):$dir sitkevij/ffmpeg -i $dir/$file1 -c:v rawvideo -pix_fmt yuv420p $dir/$file1.yuv -y
+docker run --rm -v $(pwd):$dir sitkevij/ffmpeg -i $dir/$file1 -c:v rawvideo -pix_fmt yuv420p $dir/$file1.yuv -y
 echo "** transcode $file2 to yuv"
-docker run --rm -v $(PWD):$dir sitkevij/ffmpeg -i $dir/$file2 -c:v rawvideo -pix_fmt yuv420p $dir/$file2.yuv -y
+docker run --rm -v $(pwd):$dir sitkevij/ffmpeg -i $dir/$file2 -c:v rawvideo -pix_fmt yuv420p $dir/$file2.yuv -y
 echo "** vmaf $file1 to $file2"
-docker run --rm -v $(PWD):$dir sitkevij/vmaf run_vmaf yuv420p 1920 800 $dir/$file1.yuv $dir/$file2.yuv --out-fmt json
+docker run --rm -v $(pwd):$dir sitkevij/vmaf run_vmaf yuv420p 1920 800 $dir/$file1.yuv $dir/$file2.yuv --out-fmt json
